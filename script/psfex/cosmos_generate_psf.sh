bb3a287e7d6ac045b763d4b1a496f0597262ca33
Yifei <yifei@kresge-100-64-83-92.resnet.ucsc.edu>
2021-07-08 11:22:03 -0700

    update files

--- script/psfex/cosmos_generate_psf.sh
+++ script/psfex/cosmos_generate_psf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-sex /Users/yifei/Dropbox/merian/Merian_data/COSMOS/wide/c4d_210307_021334_osj_N708_wide.fits -c cosmos_wide.sex
-
-psfex cosmos_sextractor.fits -c cosmos_wide.psfex
-
+#Building an initial model of the PSF with with SExtractor and PSFEx:
+sex /Users/yifei/Dropbox/merian/Merian_data/COSMOS/wide/c4d_210306_011257_osj_N708_deep.fits -c cosmos_deep_init.sex
+psfex cosmos_deep_init.fits -c cosmos_deep.psfex
+#Re-run the SExtractor with the PSF model
+sex -PSF_NAME cosmos_deep_init.psf /Users/yifei/Dropbox/merian/Merian_data/COSMOS/wide/c4d_210306_011257_osj_N708_deep.fits -c cosmos_deep.sex
